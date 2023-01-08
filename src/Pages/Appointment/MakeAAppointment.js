d975de8501808bb1176f0a3c124ce2ca2f5a6775
Anisur Rahman Nayan <arnayan07sept@gmail.com>
2023-01-08 21:47:25 +0600

    Updated Appointment Page

--- src/Pages/Appointment/MakeAAppointment.js
+++ src/Pages/Appointment/MakeAAppointment.js
@@ -11,8 +11,8 @@ const MakeAAppointment = ({setDate,date}) => {
           <img src={image} class="max-w-sm rounded-lg shadow-2xl" />
           <div>
             <h1 class="text-5xl font-bold">Select Your Appointment Date!</h1>
-            <p class="py-6">You must schedule an appointment for the servicing, either online using this website or through the call center</p>
-      <div className='justify-self-center'>
+            <p class="mt-3 text-black text-xl">You must schedule an appointment for the servicing, either online using this website or through the call center.</p>
+      <div >
       <DayPicker
       mode="single"
       selected={date}
