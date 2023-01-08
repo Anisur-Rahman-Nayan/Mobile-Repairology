f792228210c23a53269b603ab7e47198847fa8eb
Anisur Rahman Nayan <arnayan07sept@gmail.com>
2023-01-08 21:42:51 +0600

    Added Appointment Button

--- src/Pages/Home/Banner.js
+++ src/Pages/Home/Banner.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Banner.css'
 const Banner = () => {
     return (
@@ -8,7 +9,7 @@ const Banner = () => {
           <div class="max-w-md">
             <h1 class="mb-5 text-5xl font-bold">Mobile Repairology</h1>
             <p class="mb-5 text-xl ">Repair guides and disassembly information for phones from all manufacturers and service providers, including Apple iPhone and Samsung Galaxy. Get everything you need to repair your phone yourself</p>
-            <button class="btn btn-primary uppercase">book an appointment</button>
+           <Link to='/appointment'> <button class="btn btn-primary uppercase">book an appointment</button></Link>
           </div>
         </div>
       </div>
